a1260f1524deb342a6f700634c79cc82c095d39c
Portela10 <portelda@hotmail.es>
2021-01-29 11:07:36 +0100

    proyectos

---
+++ Lengüajes de marcas/IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 4;
+
+		if(a > b) {
+			System.out.println("a es mayor que b");
+		} else if (bZ a){
+			System.out.println("b es mayor que a");
+		}
+	}
+
+
+}
